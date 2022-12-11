9a9d49e04f2a65f5ab1387a2360585f39cb16585
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-12-11 22:54:51 +0200

    first commit before check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,11 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
-import css from '../components/App.module.css'
-
+import css from '../components/App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -23,11 +17,11 @@ export class App extends Component {
       number,
       id: nanoid(),
     };
-    
-   if(this.state.contacts.find(item => item.name === contact.name)) {
-    alert(`${contact.name} is already in contacts`)
-    return
-   }
+
+    if (this.state.contacts.find(item => item.name === contact.name)) {
+      alert(`${contact.name} is already in contacts`);
+      return;
+    }
 
     this.setState(prevState => ({
       contacts: [...prevState.contacts, contact],
@@ -41,32 +35,53 @@ export class App extends Component {
   getContacts = () => {
     const { filter, contacts } = this.state;
     const lowerCaseFilter = filter.toLowerCase();
-    
+
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(lowerCaseFilter)
     );
   };
 
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
 
-  deleteContact = (id) => {
-   this.setState(prevState => ({
-    contacts: prevState.contacts.filter(contact => contact.id !== id),
-  }));
+  componentDidMount() {
+    const contactsUnparsed = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contactsUnparsed);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
-    const {  filter } = this.state;
+    const { filter } = this.state;
     const getContacts = this.getContacts();
     return (
       <>
-      
-      <div className={css.container}>
-      <h2 className={css.title}>Phonebook</h2>
-        <ContactForm onHandleClick={this.onHandleClick} />
-        <h2 className={css.title}>Contacts</h2>
-        <Filter filter={filter} onChange={this.setFilter} />
-        <ContactList contacts={getContacts} deleteContact={this.deleteContact}/>
+        <div className={css.container}>
+          <h2 className={css.title}>Phonebook</h2>
+          <ContactForm onHandleClick={this.onHandleClick} />
+
+          <h2 className={css.title}>Contacts:</h2>
+          {this.state.contacts.length > 0 ? (
+            <Filter filter={filter} onChange={this.setFilter} />
+          ) : null}
+          {this.state.contacts.length === 0 ? (
+            <h2 className={css.title}>You have no contacts yet</h2>
+          ) : (
+            <ContactList
+              contacts={getContacts}
+              deleteContact={this.deleteContact}
+            />
+          )}
         </div>
       </>
     );
